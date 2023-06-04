cadb9b045a1e674ebb43c7c837e834bf98753415
anandjaisy <anandjaisy@gmail.com>
2023-06-04 14:06:17 +1000

    Initial setup

---
+++ apps/shop/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'https-folder-shop-entry',
+  template: `<https-folder-nx-welcome></https-folder-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
